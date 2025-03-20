938f1a1c037c659c41ecf0434123f8a64e3ca3fd
Theflamerunner89 <theflamerunner89@gmail.com>
2025-03-20 16:36:30 -0500

    Started documenting my learning process
    At this stage of the upload, I was began to utilize and understand the uses of the Math.Sqrt(), and Math,Pow() Methods to find and apply angles to find the hypotenuse of triangles.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.ReadKey();
+        }
+    }
+}
+
+//Console.WriteLine("I think this is more effecient and effective at teaching how to code in VS!");
+
+//Console.WriteLine("What is side A of the triangle?");
+//double sideA = Convert.ToDouble(Console.ReadLine());
+
+//Console.WriteLine("What is side B of the triange?");
+//double sideB = Convert.ToDouble(Console.ReadLine());
+
+//Console.WriteLine("The hypotenuse of the triangle should be " + Math.Sqrt((Math.Pow(sideA, 2) + Math.Pow(sideB, 2))));
