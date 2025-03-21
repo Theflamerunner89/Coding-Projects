f09080945dfad15390f576ceacf3a4fbda2e9737
Theflamerunner89 <theflamerunner89@gmail.com>
2025-03-21 14:16:55 -0500

    This is start of a banking application
    This is the beginnings of a Mock Banking application, applying what I know so far. Doesn't include any checking of accuracy of data. Doesn't provide any edge case detection, no implementation of balance checking.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Hello and welcome to this testng banking application. Are you a new user (y/n)");
+            string response = Console.ReadLine();
+            string userName;
+
+            if (response == "y")
+            {
+                Console.WriteLine("Welcome! please create a username.");
+                userName = Console.ReadLine(); 
+            }
+            else
+            {
+                Console.WriteLine("What is your username?");
+                userName = Console.ReadLine();
+            }
+            Console.WriteLine("Welcome " + userName + "!");
             Console.ReadKey();
         }
     }
